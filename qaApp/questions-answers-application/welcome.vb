aa886dc81ec330d663a7c21232f1b63042603a53
arshephe <andrewshepherd5396@gmail.com>
2017-11-07 12:22:10 -0600

    Bundling everything into sub folder

---
+++ qaApp/questions-answers-application/welcome.vb
@@ -0,0 +1,17 @@
+﻿
+Public Class welcome
+    Protected db As New db
+    Protected Sub LoadQuestions()
+        db.sql = "SELECT * FROM questions ORDER BY created_at DESC;"
+
+        db.fill(dgvQuestions)
+    End Sub
+    Private Sub LoadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadToolStripMenuItem.Click
+        LoadQuestions()
+    End Sub
+
+    Private Sub welcome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadQuestions()
+    End Sub
+
+End Class
